0d9a35bca2c11c2a5f95193526eff04b36d2cf08
mac <mac@Than-Namwong.local>
2024-10-22 01:12:34 +0700

    Adding the API.

--- infra/api.ts
+++ infra/api.ts
@@ -1,8 +1,27 @@
-import { bucket } from "./storage";
+// import { bucket } from "./storage";
 
-export const api = new sst.aws.ApiGatewayV2("Api");
+// export const api = new sst.aws.ApiGatewayV2("Api");
 
-api.route("GET /", {
-  link: [bucket],
-  handler: "packages/functions/src/api.handler",
+// api.route("GET /", {
+//   link: [bucket],
+//   handler: "packages/functions/src/api.handler",
+// });
+
+import { table } from "./storage";
+
+// Create the API
+export const api = new sst.aws.ApiGatewayV2("Api", {
+  transform: {
+    route: {
+      handler: {
+        link: [table],
+      },
+    }
+  }
 });
+
+api.route("POST /notes", "packages/functions/src/create.main");
+api.route("GET /notes/{id}", "packages/functions/src/get.main");
+api.route("GET /notes", "packages/functions/src/list.main");
+api.route("PUT /notes/{id}", "packages/functions/src/update.main");
+api.route("DELETE /notes/{id}", "packages/functions/src/delete.main");
